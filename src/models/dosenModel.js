a7c5a66716171ace6b294b6dacdff3242c0b7ab8
Woolf14 <gemilangtirto2002@gmail.com>
2023-06-15 15:58:29 +0700

    1

--- src/models/dosenModel.js
+++ src/models/dosenModel.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const dosenSchema = new mongoose.Schema({
   NIY: {
     type:Number,
-    required: true
+    required: true,
+    unique: true
   },
 
   nama: {
--- src/models/mahasiswaModel.js
+++ src/models/mahasiswaModel.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const mahasiswaSchema = new mongoose.Schema({
   Nim: {
     type: Number,
-    required: true
+    required: true,
+    unique: true
   },
 
   imageName: {
